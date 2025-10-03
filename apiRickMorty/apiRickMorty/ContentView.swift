c22eaaa33284a00b8702882c8e64b82a3cf1cf1f
Fenix0129 <A01384247@tec.mx>
2025-10-03 14:52:56 -0600

    Inico de proyecto
    Prueba para probar el desk del commit

---
+++ apiRickMorty/apiRickMorty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  apiRickMorty
+//
+//  Created by Alumno on 03/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apiRickMorty/apiRickMorty/apiRickMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiRickMortyApp.swift
+//  apiRickMorty
+//
+//  Created by Alumno on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct apiRickMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
