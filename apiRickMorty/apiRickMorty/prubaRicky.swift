bb4577f9f0adf21c9c790ed9984e9d517e81d5dc
Fenix0129 <A01384247@tec.mx>
2025-10-03 14:55:09 -0600

    2nda prueba

---
+++ apiRickMorty/apiRickMorty/prubaRicky.swift
@@ -0,0 +1,18 @@
+//
+//  prubaRicky.swift
+//  apiRickMorty
+//
+//  Created by Alumno on 03/10/25.
+//
+
+import SwiftUI
+
+struct prubaRicky: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    prubaRicky()
+}
